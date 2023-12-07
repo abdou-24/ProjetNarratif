1dbf87241a61290d9d32aef0c6802dc2c3839312
Abdoulaye <thierno_abdoulaye.balde@lcieducation.net>
2023-12-07 10:31:15 -0500

    Images

---
+++ Rooms/Base.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNarratif.Rooms
+{
+    internal class Base : Room
+    {
+        internal override string CreateDescription() =>;
+        internal override void ReceiveChoice(string choice)
+        {
+            switch (choice)
+            {
+                case "suite":
+                    Game.Transition<Fin>();
+                    break;
+                default:
+                    Console.WriteLine("Commande invalide.");
+                    break;
+            }
+        }
+    }
+}
